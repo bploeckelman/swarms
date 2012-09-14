4312f9d3b6cb67e84ad68652ae569432ebfbbfca
Brian Ploeckelman <ploeckelman@wisc.edu>
2012-09-13 20:08:35 -0500

    Updated main.js to make JSLint happy

--- main.js
+++ main.js
@@ -1,78 +1,96 @@
+// ----- Get JSLint to settle down ----------------------------------------
+/*jslint browser: true*/
+/*global Level*/
+/*global console*/
+
 // ----- LoadResources ----------------------------------------------------
 var images = {
-       "kalachakra" : new Image()
-     , "bubble"     : new Image()
-     , "heart"      : new Image()
-     , "textbox"    : new Image()
-     , "gold"       : new Image()
-     , "shop"       : new Image()
-     , "trees_large": new Image()
-};
-images["kalachakra" ].src = "kalachakra.png";
-images["bubble"     ].src = "bubble.png";
-images["heart"      ].src = "heart.png";
-images["textbox"    ].src = "textbox.png";
-images["gold"       ].src = "gold.png";
-images["shop"       ].src = "shop.png";
-images["trees_large"].src = "trees-large.png";
+        "kalachakra" : new Image(),
+        "bubble"     : new Image(),
+        "heart"      : new Image(),
+        "textbox"    : new Image(),
+        "gold"       : new Image(),
+        "shop"       : new Image(),
+        "trees_large": new Image()
+    };
+images.kalachakra.src  = "kalachakra.png";
+images.bubble.src      = "bubble.png";
+images.heart.src       = "heart.png";
+images.textbox.src     = "textbox.png";
+images.gold.src        = "gold.png";
+images.shop.src        = "shop.png";
+images.trees_large.src = "trees-large.png";
 
 
 // ----------------------------------------------------------------------
 //  Entry Point
 // ----------------------------------------------------------------------
 function main() {
+    "use strict";
     // ------------------------------------------------------------------
-    var reqFrame = window.requestAnimationFrame
-                || window.webkitRequestAnimationFrame
-                || window.mozRequestAnimationFrame
-                || window.oRequestAnimationFrame
-                || window. msRequestAnimationFrame
-                || function(callback, element) { 
-                        window.setTimeout(callback, 1000 / 60); 
-                   };
+    // Note: var declarations get hoisted up here anyways, 
+    //       so might as well just put them here... happy JSLint, happy life
+    var reqFrame,
+        canvas,
+        canvasWidth,
+        canvasHeight,
+        canvasBorder,
+        canvasBGColor,
+        context,
+        numTrees,
+        treeBoundary,
+        level;
 
-    var canvasWidth   = window.innerWidth - 128;
-    var canvasHeight  = window.innerHeight - 64;
-    var canvasBorder  = "rgb(175,175,125) 5px solid";
-    var canvasBGColor = "rgb(16,32,16)";
+    reqFrame = window.requestAnimationFrame
+            || window.webkitRequestAnimationFrame
+            || window.mozRequestAnimationFrame
+            || window.oRequestAnimationFrame
+            || window.msRequestAnimationFrame
+            || function (callback) {//, element) {
+            window.setTimeout(callback, 1000 / 60);
+        };
 
+    canvasWidth   = window.innerWidth - 128;
+    canvasHeight  = window.innerHeight - 64;
+    canvasBorder  = "rgb(175,175,125) 5px solid";
+    canvasBGColor = "rgb(16,32,16)";
 
-    var numTrees = 100; // for testing
-    var treeBoundary = { x: 0, y: 0, w: canvasWidth, h: canvasHeight };
-    var level = new Level(numTrees, treeBoundary);
+    numTrees = 100; // for testing
+    treeBoundary = { x: 0, y: 0, w: canvasWidth, h: canvasHeight };
+    level = new Level(numTrees, treeBoundary);
 
+    // ----- SetupResize ------------------------------------------------------
+    window.onresize = function () {
+        canvasWidth   = window.innerWidth  - 128;
+        canvasHeight  = window.innerHeight - 64;
+        canvas.width  = canvasWidth;
+        canvas.height = canvasHeight;
+        document.getElementById("container").style.width  = canvasWidth  + "px";
+        document.getElementById("container").style.height = canvasHeight + "px";
+        document.getElementById("container").style.margin = "auto auto";
+    };
 
     // ----- SetupEnvironment -------------------------------------------------
     document.getElementsByTagName("body")[0].style.background = "rgb(16,16,16)";
-    document.getElementById("container").style.width  = canvasWidth  + "px"; 
-    document.getElementById("container").style.height = canvasHeight + "px"; 
-    document.getElementById("container").style.margin = "auto auto"; 
+    document.getElementById("container").style.width  = canvasWidth  + "px";
+    document.getElementById("container").style.height = canvasHeight + "px";
+    document.getElementById("container").style.margin = "auto auto";
 
     // Setup the canvas 
-    var canvas = document.getElementById("canvas");
-    if (!canvas) console.log("Error: unable to get canvas element");
-
+    canvas = document.getElementById("canvas");
+    if (!canvas) {
+        console.log("Error: unable to get canvas element");
+    }
     canvas.width            = canvasWidth;
     canvas.height           = canvasHeight;
     canvas.style.border     = canvasBorder;
     canvas.style.background = canvasBGColor;
 
     // Setup the context
-    var context = canvas.getContext("2d");
-    if (!context) console.log("Error: unable to get canvas 2d context");
-
-
-    // ----- SetupResize ------------------------------------------------------
-    window.onresize = function(event) {
-        canvasWidth   = window.innerWidth - 128;
-        canvasHeight  = window.innerHeight - 64;
-        canvas.width  = canvasWidth;
-        canvas.height = canvasHeight;
-        document.getElementById("container").style.width  = canvasWidth  + "px"; 
-        document.getElementById("container").style.height = canvasHeight + "px"; 
-        document.getElementById("container").style.margin = "auto auto"; 
-    };
-
+    context = canvas.getContext("2d");
+    if (!context) {
+        console.log("Error: unable to get canvas 2d context");
+    }
 
     // ------------------------------------------------------------------
     function drawLoop() {
@@ -81,7 +99,7 @@ function main() {
         // Draw background image
         context.save();
         context.globalAlpha = 0.3;
-        context.drawImage(images["kalachakra"], 0, 0, canvas.width, canvas.height);
+        context.drawImage(images.kalachakra, 0, 0, canvas.width, canvas.height);
         context.restore();
 
         level.draw(context);
@@ -102,5 +120,4 @@ function main() {
 
     drawLoop();
 
-};
-
+}
