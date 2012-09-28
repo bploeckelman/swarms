b6f80ede269a34fc376d0b6f98ef20aa8313e5e6
Brian Ploeckelman <ploeckelman@wisc.edu>
2012-09-28 01:06:44 -0500

    Moved image loading to utils.js, made canvas size stay fixed

--- scripts/main.js
+++ scripts/main.js
@@ -3,44 +3,6 @@
 /*global Level*/
 /*global console*/
 
-// ----- LoadResources ----------------------------------------------------
-var images = {
-        "grass"      : new Image(),
-        "bubble"     : new Image(),
-        "heart"      : new Image(),
-        "textbox"    : new Image(),
-        "gold"       : new Image(),
-        "shop"       : new Image(),
-        "trees_large": new Image(),
-        "boid_red"   : new Image(),
-        "boid_yellow": new Image(),
-        "boid_orange": new Image(),
-        "farmer"     : new Image(),
-        "fruits"     : new Image(),
-        "apple_good" : new Image(),
-        "apple_ok"   : new Image(),
-        "apple_bad"  : new Image(),
-        "farmer12"   : new Image(),
-        "pointer"    : new Image()
-};
-images.grass.src       = "images/grass.png";
-images.bubble.src      = "images/bubble.png";
-images.heart.src       = "images/heart.png";
-images.textbox.src     = "images/textbox.png";
-images.gold.src        = "images/gold.png";
-images.shop.src        = "images/shop.png";
-images.trees_large.src = "images/trees-large.png";
-images.boid_red.src    = "images/boid-red.png";
-images.boid_yellow.src = "images/boid-yellow.png";
-images.boid_orange.src = "images/boid-orange.png";
-images.farmer.src      = "images/farmer.png";
-images.fruits.src      = "images/pacman-fruit.png";
-images.apple_good.src  = "images/apple-good.png";
-images.apple_ok.src    = "images/apple-dropped.png";
-images.apple_bad.src   = "images/apple-rotten.png";
-images.farmer12.src    = "images/farmer12.png";
-images.pointer.src     = "images/pointer.png";
-
 var keyState = {};
 window.addEventListener('keydown', function(e) {
     keyState[e.keycode || e.which] = true;
@@ -77,8 +39,8 @@ window.addEventListener('keyup', function(e) {
             window.setTimeout(callback, 1000 / 60);
         };
 
-    canvasWidth   = window.innerWidth - 128;
-    canvasHeight  = window.innerHeight - 64;
+    canvasWidth   = 800;
+    canvasHeight  = 600;
     canvasBorder  = "rgb(175,175,125) 5px solid";
     canvasBGColor = "rgb(16,32,16)";
 
@@ -86,16 +48,6 @@ window.addEventListener('keyup', function(e) {
     treeBoundary = { x: 100, y: 100, w: canvasWidth - 100, h: canvasHeight - 100 };
     level = new Level(numTrees, treeBoundary);
 
-    // ----- SetupResize ------------------------------------------------------
-    window.onresize = function () {
-        canvasWidth   = window.innerWidth  - 128;
-        canvasHeight  = window.innerHeight - 64;
-        canvas.width  = canvasWidth;
-        canvas.height = canvasHeight;
-        document.getElementById("container").style.width  = canvasWidth  + "px";
-        document.getElementById("container").style.height = canvasHeight + "px";
-        document.getElementById("container").style.margin = "auto auto";
-    };
 
     // ----- SetupEnvironment -------------------------------------------------
     document.getElementsByTagName("body")[0].style.background = "rgb(16,16,16)";
@@ -120,7 +72,8 @@ window.addEventListener('keyup', function(e) {
     }
 
     // ------------------------------------------------------------------
-    /*canvas.addEventListener("click", function (event) {
+    canvas.addEventListener("click", function (event) {
+        /*
         var gascloud = level.farmer.handleClick({
             x: event.pageX - canvas.offsetLeft,
             y: event.pageY - canvas.offsetTop
@@ -128,7 +81,10 @@ window.addEventListener('keyup', function(e) {
         if (gascloud !== null) {
             level.gasclouds.push(gascloud);
         }
-    }, false);*/
+        */
+        // TESTING: This kills the farmer
+        level.farmer.health = 0;
+    }, false);
 
     // ------------------------------------------------------------------
     // ------------------------------------------------------------------
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,40 @@
+var images = {
+        "grass"      : new Image(),
+        "bubble"     : new Image(),
+        "heart"      : new Image(),
+        "textbox"    : new Image(),
+        "gold"       : new Image(),
+        "shop"       : new Image(),
+        "trees_large": new Image(),
+        "boid_red"   : new Image(),
+        "boid_yellow": new Image(),
+        "boid_orange": new Image(),
+        "farmer"     : new Image(),
+        "fruits"     : new Image(),
+        "apple_good" : new Image(),
+        "apple_ok"   : new Image(),
+        "apple_bad"  : new Image(),
+        "farmer12"   : new Image(),
+        "pointer"    : new Image()
+};
+images.grass.src       = "images/grass.png";
+images.bubble.src      = "images/bubble.png";
+images.heart.src       = "images/heart.png";
+images.textbox.src     = "images/textbox.png";
+images.gold.src        = "images/gold.png";
+images.shop.src        = "images/shop.png";
+images.trees_large.src = "images/trees-large.png";
+images.boid_red.src    = "images/boid-red.png";
+images.boid_yellow.src = "images/boid-yellow.png";
+images.boid_orange.src = "images/boid-orange.png";
+images.farmer.src      = "images/farmer.png";
+images.fruits.src      = "images/pacman-fruit.png";
+images.apple_good.src  = "images/apple-good.png";
+images.apple_ok.src    = "images/apple-dropped.png";
+images.apple_bad.src   = "images/apple-rotten.png";
+images.farmer12.src    = "images/farmer12.png";
+images.pointer.src     = "images/pointer.png";
+
 // ----------------------------------------------------------------------------
 //     HUD Utils
 // ----------------------------------------------------------------------------
